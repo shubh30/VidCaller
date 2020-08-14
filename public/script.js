2dec5ec8c4c07636c7a64d3b85b2678ce30de3bf
Shubham <shubhamnagpal30@gmail.com>
2020-08-14 18:12:05 +0530

    Join two users in a room using Socket.io

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+const socket = io('/');
 const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
 myVideo.muted = true;
@@ -12,6 +13,16 @@ navigator.mediaDevices
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
   });
+
+socket.emit('join-room', ROOM_ID);
+
+socket.on('user-connected', () => {
+  connectToNewUser();
+})
+
+const connectToNewUser = () => {
+  console.log('New User');
+}
 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const io = require('socket.io')(server);
 const { v4: uuidv4 } = require('uuid');
 
 app.set('view engine', 'ejs');
@@ -14,5 +15,12 @@ app.get('/', (req, res) => {
 app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room });
 });
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected');
+  })
+})
 
 server.listen(3030);
