2703527cd17483c8f1ba3025e0cf50fb05908148
Shubham <shubhamnagpal30@gmail.com>
2020-08-14 17:29:48 +0530

    Add the ability to view video of myself

--- sever.js
+++ sever.js
@@ -1,16 +1,18 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const server = require("http").Server(app);
-const { v4: uuidv4 } = require("uuid");
+const server = require('http').Server(app);
+const { v4: uuidv4 } = require('uuid');
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
   res.redirect(`/${uuidv4()}`);
 });
 
-app.get("/:room", (req, res) => {
-  res.render("room", { roomId: req.params.room });
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room });
 });
 
 server.listen(3030);
